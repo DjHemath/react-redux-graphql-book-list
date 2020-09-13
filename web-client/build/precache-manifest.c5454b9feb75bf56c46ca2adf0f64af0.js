1d6a897e4b3823d6946b62120dd2af5f371b178f
DJ Hemath <djhemath@gmail.com>
2020-09-13 16:00:42 +0530

    Fixed that gitignore on build folder for deployment

---
+++ web-client/build/precache-manifest.c5454b9feb75bf56c46ca2adf0f64af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6602a071f0baf7377031b6f75ba00d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb5f9e19053395a2bc1a",
+    "url": "/static/css/main.e80fbf08.chunk.css"
+  },
+  {
+    "revision": "e1ceeff36f73176ca778",
+    "url": "/static/js/2.35676602.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.35676602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5f9e19053395a2bc1a",
+    "url": "/static/js/main.51dabf58.chunk.js"
+  },
+  {
+    "revision": "a875509d226f5e6d12fd",
+    "url": "/static/js/runtime-main.b14accd2.js"
+  }
+]);
